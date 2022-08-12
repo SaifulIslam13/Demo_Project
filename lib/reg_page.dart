c11cf84c0b90bcfadeac8090d9a943dfab377616
Saiful Islam <saiful1561997@gmail.com>
2022-08-12 21:49:24 +0600

    Bug Fixes

--- lib/reg_page.dart
+++ lib/reg_page.dart
@@ -16,61 +16,41 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-                height: 400,
-                width: double.infinity,
-                child: Image(
-                  image: AssetImage("images/image4.jpg"),
-                  fit: BoxFit.cover,
-                )),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Welcome to\nRegistration Page",
-              style: TextStyle(
-                  fontFamily: "fonts1",
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.orange),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Please write the correct information",
-              style: TextStyle(color: Colors.blue, fontFamily: "fonts1"),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                icon: Container(
-                    height: 40,
-                    width: 40,
-                    decoration: BoxDecoration(
-                        color: Colors.orange,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Icon(
-                      Icons.email_outlined,
-                      color: Colors.white,
-                      size: 25,
-                    )),
-                hintText: "Email",
-                hintStyle: TextStyle(color: Colors.blue),
+          body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  height: 400,
+                  width: double.infinity,
+                  child: Image(
+                    image: AssetImage("images/image4.jpg"),
+                    fit: BoxFit.cover,
+                  )),
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: TextFormField(
+              Text(
+                "Welcome to\nRegistration Page",
+                style: TextStyle(
+                    fontFamily: "fonts1",
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.orange),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Please write the correct information",
+                style: TextStyle(color: Colors.blue, fontFamily: "fonts1"),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
                 decoration: InputDecoration(
                   icon: Container(
                       height: 40,
@@ -79,54 +59,76 @@ class _LoginPageState extends State<LoginPage> {
                           color: Colors.orange,
                           borderRadius: BorderRadius.circular(10)),
                       child: Icon(
-                        Icons.lock,
+                        Icons.email_outlined,
                         color: Colors.white,
-                        size: 30,
+                        size: 25,
                       )),
-                  suffixIcon: Icon(Icons.visibility),
-                  hintText: "Password",
+                  hintText: "Email",
                   hintStyle: TextStyle(color: Colors.blue),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Center(
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => PhoneVerify()));
-                    },
-                    child: Text("Continue"))),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Text(
-                  "Already Have an Account?",
-                  style: TextStyle(fontFamily: "fonts1", fontSize: 16),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    icon: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Icon(
+                          Icons.lock,
+                          color: Colors.white,
+                          size: 30,
+                        )),
+                    suffixIcon: Icon(Icons.visibility),
+                    hintText: "Password",
+                    hintStyle: TextStyle(color: Colors.blue),
+                  ),
                 ),
-                InkWell(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: ((context) {
-                        return SigninPage();
-                      })));
-                    },
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.red),
-                    ))
-              ],
-            )
-          ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => PhoneVerify()));
+                      },
+                      child: Text("Continue"))),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Already Have an Account?",
+                    style: TextStyle(fontFamily: "fonts1", fontSize: 16),
+                  ),
+                  InkWell(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: ((context) {
+                          return SigninPage();
+                        })));
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red),
+                      ))
+                ],
+              )
+            ],
+          ),
         ),
       )),
     );
